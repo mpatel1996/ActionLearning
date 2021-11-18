e562faba4565390006f445d0fbc03ffc8cb56ef9
mpatel1996 <mpjv12343@gmail.com>
2021-11-18 16:45:08 -0600

    Added JS Tutorial

---
+++ JS Tutorials/test.js
@@ -0,0 +1,54 @@
+//------------------------------------
+// Creating Objects
+// one way to create an object
+/*
+console.log("**Learning how to create objects: ");
+var myCar = new Object();
+myCar.driver = "mihir";
+myCar.drive = function(){
+    console.log("Now Driving");
+};
+
+myCar.drive();
+
+
+// Second way to create an object
+
+var mycar2 = {
+    maxSpeed : 39,
+    driver : "Patel",
+    drive : function(){
+        console.log("Now Driving 2");
+    },
+    logDriver: function(){
+        console.log("Driver name is " + this.driver);
+    }
+};
+
+mycar2.drive();
+mycar2.logDriver();
+*/
+
+//------------------------------------
+
+
+//------------------------------------
+// Creating constructors 
+console.log("\n\n**Creating Constructors:");
+var Car = function(maxSpeed, driver){
+    this.maxSpeed = maxSpeed;
+    this.driver = driver;
+    this.drive = function(speed, time){
+        console.log(speed * time); 
+    };
+    this.logDriver = function(){
+        console.log("Driver name is " + this.driver);
+    };
+};
+
+var myCar = new Car(50, "Ninja");
+var myCar2 = new Car(510, "Ginja");
+var myCar3 = new Car(540, "Dinja");
+
+myCar.drive(30,5);
+myCar3.logDriver();
