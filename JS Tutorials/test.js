14f19e9fd9ad803ea435ec78cf22fb754a953657
mpatel1996 <mpjv12343@gmail.com>
2021-11-22 10:50:53 -0600

    added rectFrame ()

--- JS Tutorials/test.js
+++ JS Tutorials/test.js
@@ -55,14 +55,57 @@ mycar2.logDriver();
 // *****************************
 // JS Assignments
 // -----------------------------
-function addFunc() {
-  var n = parseInt(prompt("Enter a number"));
-  var sum = 0;
-  for (i = 1; i <= n; i++) {
-    if((i / 3 == 1) || (i / 5 == 1)){
-        sum += i;
+// function addFunc() {
+//   var n = parseInt(prompt("Enter a number"));
+//   var sum = 0;
+//   for (i = 1; i <= n; i++) {
+//     if((i / 3 == 1) || (i / 5 == 1)){
+//         sum += i;
+//     }
+//   }
+//   console.log(sum);
+// }
+// addFunc();
+
+// ******************************
+// Even of Odd
+// ------------------------------
+// function oddOrEven(n){
+//     if ( n % 2 ==0){
+//         return "Even";
+//     }
+//     return "Odd";
+// }
+
+// document.getElementById("hello").innerHTML = oddOrEven(4);
+
+
+
+// *******************************
+// Rectangle Frame
+// -------------------------------
+function ast(len){
+    var startEnd = "";
+    for (i = 0; i < len; i++){
+        startEnd += "* ";
     }
-  }
-  console.log(sum);
+    return startEnd;
 }
-addFunc();
+
+function getLongest(arr){
+    return arr.reduce((x,b) => x.length < b.length ? b : x, "");
+}
+ 
+function rectFrame(a){
+    var newArr = "";
+    var startEnd = ast(getLongest(a).length);
+    newArr = (startEnd + "\n");
+    for (j = 0; j < a.length; j++){
+        newArr += "* " + a[j] +" *\n";
+    };
+    newArr += (startEnd + "\n");
+    console.log(newArr);
+    return newArr;
+}
+var a = ["hello", 'world','this','is','Mihir', 'Patel'];
+rectFrame(a);
