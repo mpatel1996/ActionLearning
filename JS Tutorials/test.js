561437504e6f990669fcddb32672287eff56b213
mpatel1996 <mpjv12343@gmail.com>
2021-11-22 09:52:33 -0600

    added addFunc

--- JS Tutorials/test.js
+++ JS Tutorials/test.js
@@ -31,24 +31,38 @@ mycar2.logDriver();
 
 //------------------------------------
 
-
 //------------------------------------
-// Creating constructors 
-console.log("\n\n**Creating Constructors:");
-var Car = function(maxSpeed, driver){
-    this.maxSpeed = maxSpeed;
-    this.driver = driver;
-    this.drive = function(speed, time){
-        console.log(speed * time); 
-    };
-    this.logDriver = function(){
-        console.log("Driver name is " + this.driver);
-    };
-};
+// Creating constructors
+// console.log("\n\n**Creating Constructors:");
+// var Car = function(maxSpeed, driver){
+//     this.maxSpeed = maxSpeed;
+//     this.driver = driver;
+//     this.drive = function(speed, time){
+//         console.log(speed * time);
+//     };
+//     this.logDriver = function(){
+//         console.log("Driver name is " + this.driver);
+//     };
+// };
 
-var myCar = new Car(50, "Ninja");
-var myCar2 = new Car(510, "Ginja");
-var myCar3 = new Car(540, "Dinja");
+// var myCar = new Car(50, "Ninja");
+// var myCar2 = new Car(510, "Ginja");
+// var myCar3 = new Car(540, "Dinja");
 
-myCar.drive(30,5);
-myCar3.logDriver();
+// myCar.drive(30,5);
+// myCar3.logDriver();
+
+// *****************************
+// JS Assignments
+// -----------------------------
+function addFunc() {
+  var n = parseInt(prompt("Enter a number"));
+  var sum = 0;
+  for (i = 1; i <= n; i++) {
+    if((i / 3 == 1) || (i / 5 == 1)){
+        sum += i;
+    }
+  }
+  console.log(sum);
+}
+addFunc();
