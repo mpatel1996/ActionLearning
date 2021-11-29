f4b8342c7e37c2e1f2d3db9ec2d5f232d7727058
mpatel1996 <mpjv12343@gmail.com>
2021-11-29 11:24:56 -0600

    Color wheel example

--- TS Tutorials/scripts/demo.js
+++ TS Tutorials/scripts/demo.js
@@ -1,3 +1,15 @@
 "use strict";
-console.log("hello world!");
+// ...colors in function argument = REST Operator
+let showColor = function (message, ...color) {
+    console.log(message, color);
+    for (let i in color) {
+        console.log(color[i]);
+    }
+};
+var message = "List of color";
+let colArr = ["red", "blue", "green"];
+// Example to show colors became an array during fuction argument 
+showColor(message, "pink", "yellow", "Cayan");
+// ...colArr in function call = Spread Operator
+showColor(message, ...colArr);
 //# sourceMappingURL=demo.js.map
