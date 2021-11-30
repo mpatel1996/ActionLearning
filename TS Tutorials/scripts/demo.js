fe339031b7247b971a4786407182ea4e0ae22535
mpatel1996 <mpjv12343@gmail.com>
2021-11-30 17:21:38 -0600

    updated scripts folder

--- TS Tutorials/scripts/demo.js
+++ TS Tutorials/scripts/demo.js
@@ -1,15 +1,28 @@
 "use strict";
-// ...colors in function argument = REST Operator
-let showColor = function (message, ...color) {
-    console.log(message, color);
-    for (let i in color) {
-        console.log(color[i]);
+// // ...colors in function argument = REST Operator
+// let showColor = function (message, ...color) {
+//   console.log(message, color);
+//   for (let i in color) {
+//     console.log(color[i]);
+//   }
+// };
+// var message = "List of color";
+// let colArr = ["red", "blue", "green"];
+// // Example to show colors became an array during fuction argument
+// showColor(message, "pink", "yellow", "Cayan");
+// // ...colArr in function call = Spread Operator
+// showColor(message, ...colArr);
+let arr = ["hello", "world", "This Mihir"];
+var na = (arr) => {
+    let newArr = []; // = [] as any; removed " Type 'any' is not assignable to type 'never'. issue"
+    for (var i = 0; i < arr.length; i++) {
+        newArr.push({
+            name: arr[i],
+            length: arr[i].length
+        });
     }
+    ;
+    return newArr;
 };
-var message = "List of color";
-let colArr = ["red", "blue", "green"];
-// Example to show colors became an array during fuction argument 
-showColor(message, "pink", "yellow", "Cayan");
-// ...colArr in function call = Spread Operator
-showColor(message, ...colArr);
+console.log(na(arr));
 //# sourceMappingURL=demo.js.map
