444eaea8435853b1340ac4c791b7b10d1d0823d3
mpatel1996 <mpjv12343@gmail.com>
2021-11-30 17:20:59 -0600

    changing array to new obj arr example

--- TS Tutorials/demo.ts
+++ TS Tutorials/demo.ts
@@ -1,15 +1,29 @@
-// ...colors in function argument = REST Operator
-let showColor = function (message, ...color) {
-  console.log(message, color);
-  for (let i in color) {
-    console.log(color[i]);
-  }
-};
+// // ...colors in function argument = REST Operator
+// let showColor = function (message, ...color) {
+//   console.log(message, color);
+//   for (let i in color) {
+//     console.log(color[i]);
+//   }
+// };
 
-var message = "List of color";
-let colArr = ["red", "blue", "green"];
+// var message = "List of color";
+// let colArr = ["red", "blue", "green"];
 
-// Example to show colors became an array during fuction argument
-showColor(message, "pink", "yellow", "Cayan");
-// ...colArr in function call = Spread Operator
-showColor(message, ...colArr);
+// // Example to show colors became an array during fuction argument
+// showColor(message, "pink", "yellow", "Cayan");
+// // ...colArr in function call = Spread Operator
+// showColor(message, ...colArr);
+
+let arr = ["hello", "world","This Mihir"];
+var na = (arr) => {
+  let newArr = []; // = [] as any; removed " Type 'any' is not assignable to type 'never'. issue"
+  for (var i = 0; i < arr.length; i++) {
+    newArr.push({
+      name: arr[i],
+      length: arr[i].length
+    });
+  };
+  
+  return newArr;
+}
+console.log(na(arr));
